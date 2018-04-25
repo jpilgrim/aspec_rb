ad17424ae0678986cbc38c26bf5dff3fd54186bb
tcob <briantcob@gmail.com>
2018-04-25 21:21:27 +0200

    rename tests

---
+++ test/test_suite.rb
@@ -0,0 +1,12 @@
+require_relative 'test_test_helper'
+
+# require_relative 'test_convert_doc'
+require_relative 'test_definition_block'
+require_relative 'test_inline_callout_macro'
+require_relative 'test_inline_task_macro'
+require_relative 'test_inline_repo_macro'
+require_relative 'test_inline_cwiki_macro'
+# require_relative 'test_requirement_appendix'
+require_relative 'test_requirement_block'
+require_relative 'test_todo_block'
+# require_relative 'test_html_postprocessor'
