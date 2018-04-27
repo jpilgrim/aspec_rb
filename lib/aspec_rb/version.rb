273983b0878f360dd8a1557d81c55f698bb8f37f
bsmith <brian.smith@numberfour.eu>
2018-04-27 13:20:58 +0200

    0.0.12

--- lib/aspec_rb/version.rb
+++ lib/aspec_rb/version.rb
@@ -7,5 +7,5 @@ module AspecRb
   # For this deploy config, see https://github.com/tcob/aspec_rb/blob/master/.travis.yml
   #
   # Manual release can be performed by running 'bundle install && rake release'
-  VERSION = '0.0.11'
+  VERSION = '0.0.12'
 end
