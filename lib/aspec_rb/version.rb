3a924de734791d7a7e01e15597fffd69de02c6ae
bsmith <brian.smith@numberfour.eu>
2018-06-06 17:41:44 +0200

    0.0.15 bsearch, benchmark

--- lib/aspec_rb/version.rb
+++ lib/aspec_rb/version.rb
@@ -7,5 +7,5 @@ module AspecRb
   # For this deploy config, see https://github.com/tcob/aspec_rb/blob/master/.travis.yml
   #
   # Manual release can be performed by running 'bundle install && rake release'
-  VERSION = '0.0.14'
+  VERSION = '0.0.15'
 end
--- lib/extensions/autoxrefs.rb
+++ lib/extensions/autoxrefs.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'asciidoctor/extensions'
+require 'benchmark'
 require_relative 'utils/utils'
 
 include ::Asciidoctor
@@ -12,151 +13,172 @@ AnchorRx = /\[\[(?:|([\w+?_:][\w+?:.-]*)(?:, *(.+))?)\]\]/
 
 ni_includes, includes, doclinks, anchorfixes, intrachapter, interchapter, anchors, xrefs = Array.new(9) { [] }
 
+# Don't do this!
 adoc_files = Dir.glob("#{$srcdir}/**/*.adoc")
 
 indexincludes = Index.includes
 
-adoc_files.each do |filename|
-  main = false
-  path = Sform.trim(filename)
-  chapter = path.match(/.+?(?=\/)/).to_s
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - Scanning Source Files') do
+    adoc_files.each do |filename|
+      main = false
+      path = Sform.trim(filename)
+      chapter = path.match(/.+?(?=\/)/).to_s
 
-  # Add a switch if the current document is an include within the index.adoc
-  indexincludes.each do |inc|
-    main = true if inc == filename
-  end
+      # Add a switch if the current document is an include within the index.adoc
+      indexincludes.each do |inc|
+        main = true if inc == filename
+      end
 
-  File.read(filename).each_line do |li|
-    h1 = false
+      File.read(filename).each_line do |li|
+        h1 = false
 
-    if li[/\<\<(?!Req)(.+?)\>\>/]
-      # Handle multiple cross refs per line
-      li.scan(/(?=\<\<(?!Req)(.+?)\>\>)/) do |xref|
-        text = ''
-        target = ''
-        xref = xref[0].to_s
-        target = xref.gsub(/\s/, '-')
+        if li[/\<\<(?!Req)(.+?)\>\>/]
+          # Handle multiple cross refs per line
+          li.scan(/(?=\<\<(?!Req)(.+?)\>\>)/) do |xref|
+            text = ''
+            target = ''
+            xref = xref[0].to_s
+            target = xref.gsub(/\s/, '-')
 
-        if xref[/,/]
-          target = xref.gsub(/,.+/, '')
-          text = xref.gsub(/.+,/, '').lstrip
-          xref = xref.sub(/,.+/, '')
-        else
-          text = Sform.titleify(xref).strip
+            if xref[/,/]
+              target = xref.gsub(/,.+/, '')
+              text = xref.gsub(/.+,/, '').lstrip
+              xref = xref.sub(/,.+/, '')
+            else
+              text = Sform.titleify(xref).strip
+            end
+            xrefs.push([xref, path, filename, text, target, chapter])
+          end
+
+        elsif li[/^(\=+\s+?\S+.+)/]
+          h1 = true if li[/^=\s+?\S+.+/]
+          title = li.chop.match(/(?!=+\s)(\S+.+?)$/i).captures[0].strip
+          title.sub!(/\.(?=\w+?)/, '') if title[/\.(?=\w+?)/]
+          link = Sform.underscorify(title)
+          anchors.push([title, path, filename, link, chapter, main, h1])
+
+        # Handle images separately
+        elsif li[/^(\.\S\w+)/]
+          title = li.chop.match(/(?!=+\s)(\S+.+?)$/i).captures[0].strip
+          title.sub!(/\.(?=\w+?)/, '') if title[/\.(?=\w+?)/]
+          anchors.push([title, path, filename, title, chapter, main, h1])
+
+        elsif li[/\[\[(?:|([\w+?_:][\w+?:.-]*)(?:, *(.+))?)\]\]/]
+          anchor = li.chop.match(/(?<=\[\[).+?(?=\]\])/).to_s
+
+          if anchor[/,/]
+            anchor = anchor.match(/(?<=\[\[)(?:|[\w+?_:][\w+?:.-]*)(?=,.+?\]\])/).to_s
+            text = anchor.sub(/.+?,/, '')
+            text = text.sub(/\]\]$/, '')
+          else
+            text = anchor
+          end
+
+          anchors.push([anchor, path, filename, text, chapter, main, h1, true])
+
+        # Match for sub includes
+        elsif li[IncludeDirectiveRx]
+          child = li.match(/(?<=^include::).+?\.adoc(?=\[\])/).to_s
+          child = child.sub(/^\{find\}/, '')
+          childpath = "#{filename.sub(/[^\/]+?\.adoc/, '')}#{child}"
+          includes.push([Sform.trim(filename), child, Sform.trim(childpath)])
+
         end
-        xrefs.push([xref, path, filename, text, target, chapter])
       end
-
-    elsif li[/^(\=+\s+?\S+.+)/]
-      h1 = true if li[/^=\s+?\S+.+/]
-      title = li.chop.match(/(?!=+\s)(\S+.+?)$/i).captures[0].strip
-      title.sub!(/\.(?=\w+?)/, '') if title[/\.(?=\w+?)/]
-      link = Sform.underscorify(title)
-      anchors.push([title, path, filename, link, chapter, main, h1])
-
-    # Handle images separately
-    elsif li[/^(\.\S\w+)/]
-      title = li.chop.match(/(?!=+\s)(\S+.+?)$/i).captures[0].strip
-      title.sub!(/\.(?=\w+?)/, '') if title[/\.(?=\w+?)/]
-      anchors.push([title, path, filename, title, chapter, main, h1])
-
-    elsif li[/\[\[(?:|([\w+?_:][\w+?:.-]*)(?:, *(.+))?)\]\]/]
-      anchor = li.chop.match(/(?<=\[\[).+?(?=\]\])/).to_s
-
-      if anchor[/,/]
-        anchor = anchor.match(/(?<=\[\[)(?:|[\w+?_:][\w+?:.-]*)(?=,.+?\]\])/).to_s
-        text = anchor.sub(/.+?,/, '')
-        text = text.sub(/\]\]$/, '')
-      else
-        text = anchor
-      end
-
-      anchors.push([anchor, path, filename, text, chapter, main, h1, true])
-
-    # Match for sub includes
-    elsif li[IncludeDirectiveRx]
-      child = li.match(/(?<=^include::).+?\.adoc(?=\[\])/).to_s
-      child = child.sub(/^\{find\}/, '')
-      childpath = "#{filename.sub(/[^\/]+?\.adoc/, '')}#{child}"
-      includes.push([filename, child, childpath])
-
     end
   end
 end
 
-# Create array of non-indexed includes
-adoc_files.each do |filename|
-  includes.each do |parent, child, childpath|
-    next unless childpath == filename
-    ni_includes.push([parent, child, filename])
+
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - Pushing nested includes') do
+    # Create array of non-indexed includes
+    adoc_files.each do |filename|
+      includes.each do |parent, child, childpath|
+        next unless childpath == filename
+        ni_includes.push([parent, child, filename])
+      end
+    end
   end
 end
 
-# For each main include, store a target link
-anchors.each do |_anchor, full, _filename, link, chapter, main, h1|
-  next unless main && h1
-  doc = full.gsub(/^#{chapter}\//, '')
-  doclinks.push([doc, link, chapter])
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - For each main include, store a target link') do
+    # For each main include, store a target link
+    anchors.each do |_anchor, full, _filename, link, chapter, main, h1|
+      next unless main && h1
+      doc = full.gsub(/^#{chapter}\//, '')
+      doclinks.push([doc, link, chapter])
+    end
+  end
 end
 
-# If a section title has an overriding anchor on the previous line, perform the following fix
 o_anchors = []
-doclinks.delete_if do |doc, _link, mchapter|
-  topleveldoc = "#{$srcdir}/#{mchapter}/#{doc}.adoc"
-  lines = File.foreach(topleveldoc).first(10).join
-  next unless lines[/(?x)\[\[.+?\]\]\n=\s{1,}.+$/]
-  overriding_anchor = lines.match(/(?x)\[\[(.+?)\]\]\n=\s{1,}.+$/).captures[0].to_s
-  o_anchors.push([doc, overriding_anchor, mchapter])
-  true
+
+
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - Overriding Anchors') do
+    # If a section title has an overriding anchor on the previous line, perform the following fix
+    doclinks.delete_if do |doc, _link, mchapter|
+      topleveldoc = "#{$srcdir}/#{mchapter}/#{doc}.adoc"
+      lines = File.foreach(topleveldoc).first(10).join
+      next unless lines[/(?x)\[\[.+?\]\]\n=\s{1,}.+$/]
+      overriding_anchor = lines.match(/(?x)\[\[(.+?)\]\]\n=\s{1,}.+$/).captures[0].to_s
+      o_anchors.push([doc, overriding_anchor, mchapter])
+      true
+    end
+  end
 end
 
 doclinks += o_anchors
 doclinks.uniq!
 
-# Edit the array of Anchors to point to the parent document *if* it is included.
-# TODO use a while loop, repeat until no changes made
-tempanchors = []
-3.times do
-  tempanchors.clear
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - Point include to parent') do
+    includes.each do |parent, _child, childpath|
+      anchors.map! { |anchor, path, filename, text, chapter, main, _h1|
+        filename = parent if childpath == path
+        [anchor, path, filename, text, chapter, main, _h1]
+      }
+    end
+    anchors.uniq!
+  end
+end
 
-  # Loop through all includes, if the anchor is contained in an include,
-  # edit the anchors array to point to its parent instead
-  includes.each do |parent, _child, childpath|
-    anchors.delete_if do |anchor, path, filename, text, chapter, main, _h1|
-      next unless Sform.trim(childpath) == Sform.trim(filename)
-      tempanchors.push([anchor, path, Sform.trim(parent), text, chapter, main])
-      true
+tempanchors = []
+
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - tempanchors') do
+    anchors.delete_if do |anchor, apath, trim_parent, parent, amain, achapter|
+      doclinks.each do |doc, link, dchapter|
+        next unless apath == "#{dchapter}/#{doc}" || trim_parent == "#{dchapter}/#{doc}"
+        tempanchors.push([anchor, apath, link, parent, amain, achapter])
+        true
+      end
     end
   end
-
-  anchors += tempanchors
-  anchors.uniq!
 end
 
-tempanchors.clear
-
-anchors.delete_if do |anchor, apath, trim_parent, parent, amain, achapter|
-  doclinks.each do |doc, link, dchapter|
-    next unless apath == "#{dchapter}/#{doc}" || trim_parent == "#{dchapter}/#{doc}"
-    tempanchors.push([anchor, apath, link, parent, amain, achapter])
-    true
-  end
-end
-
+tempanchors.uniq!
 anchors += tempanchors
 anchors.uniq!
 
-# For all requirements, check which chapter they should finally be in with includes catered for
-# match with a main document name - should be a main chapter title
-xrefs.each do |xref, xpath, _xfilename, _xtext, xtarget, _xchapter|
-  anchors.each do |anchor, apath, afilename, atext, _achapter, _amain, _h1|
-    next unless xref == anchor
-    # if in same chapter, dont link to other HTML file
-    afilename = '' if xpath == apath
-    # xtext = xref if xtext.empty?
-    afilename.sub!(/^_/, '') if afilename[/^_/]
-    fix = "#{afilename}##{atext},#{xtarget}"
-    anchorfixes.push([anchor, fix, xref])
+Benchmark.bm do |bm|
+  bm.report('Autoxrefs - Create a final array of fixes') do
+    # For all requirements, check which chapter they should finally be in with includes catered for
+    # match with a main document name - should be a main chapter title
+    xrefs.each do |xref, xpath, _xfilename, _xtext, xtarget, _xchapter|
+      anchors.each do |anchor, apath, afilename, atext, _achapter, _amain, _h1|
+        next unless xref == anchor
+        # if in same chapter, dont link to other HTML file
+        afilename = '' if xpath == apath
+        # xtext = xref if xtext.empty?
+        afilename.sub!(/^_/, '') if afilename[/^_/]
+        fix = "#{afilename}##{atext},#{xtarget}"
+        anchorfixes.push([anchor, fix, xref])
+      end
+    end
   end
 end
 
